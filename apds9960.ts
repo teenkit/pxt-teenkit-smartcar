042ca524744cff685fd952c0c3f1bc74dd511c5c
limaodong118 <43627627+limaodong118@users.noreply.github.com>
2018-12-13 22:31:07 +0800

    Update apds9960.ts

--- apds9960.ts
+++ apds9960.ts
@@ -240,10 +240,8 @@ namespace ZjwlGesture {
             . # # # .
             `);
         basic.pause(5000);
-        let apds9960 = new APDS9960();
-        apds9960.init();
+
         serial.writeLine("chushihuawancheng")
-        //let a = apds9960.read()
 
         basic.showLeds(`
                 . . . . .
@@ -252,8 +250,6 @@ namespace ZjwlGesture {
                 # . # . #
                 . # # # .
                 `);
-       // basic.pause(200)
-       // serial.writeLine(a.toString())
     }
 
     /* Container for gesture data */
