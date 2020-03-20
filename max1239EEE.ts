21f4c07dbf041f01eb5747d732d4fc68caf93000
birchie liu <56035255+teenkit@users.noreply.github.com>
2020-03-20 19:56:19 +0800

    Update max1239EEE.ts

--- max1239EEE.ts
+++ max1239EEE.ts
@@ -22,16 +22,16 @@ enum MyEnum {
 
 //% weight=100 color=#0fbc11 icon="\uf013"
 namespace smartcar {
-    let MAX1239 = 0x35;
+    let MAX1399 = 0x35;
     /**
      * setup字节，配置为：11110010。即： 使用内部参考电压，并保持参考电压开启
      */
     let setup_byte = 0xf2;
     /**
-     * config字节，配置为： 00000101。 即采取从AIN0扫描到AIN7的扫描模式，和single-end的转换模式
+     * config字节，配置为： 01010101。 即采取从AIN6扫描到AIN10的扫描模式，和single-end的转换模式
      * TODO： 电路上需要对遮挡传感器和光敏电阻进行分组，此配置需要修改，并本别为两种传感器分组。
      */
-    let config_byte = 0x5; //只读取三个遮挡传感器
+    let config_byte = 0x55;
 
     /**
      * 只读取AIN0的配置:01100001
@@ -46,30 +46,30 @@ namespace smartcar {
         let buf = pins.createBuffer(2);
         buf.setNumber(NumberFormat.UInt8BE, 0, setup_byte);
         buf.setNumber(NumberFormat.UInt8BE, 1, config_byte);
-        pins.i2cWriteBuffer(MAX1239, buf);
+        pins.i2cWriteBuffer(MAX1399, buf);
 
-        serial.writeLine("max1239 initialized");
+        serial.writeLine("max1399 initialized");
     }
 
     //% blockId="INFRARED_LIGHT_READ" block="读取全部数据"
     //% weight=61 blockGap=8
     export function read() {
-        let buf = pins.createBuffer(3);
+        let buf = pins.createBuffer(8);
 
         for (; ;) {
-            buf = pins.i2cReadBuffer(MAX1239, 6);
+            buf = pins.i2cReadBuffer(MAX1399, 10);
 
-            let i1 = buf.getNumber(NumberFormat.UInt16BE, 0) & 0xfff;
-            let i2 = buf.getNumber(NumberFormat.UInt16BE, 2) & 0xfff;
-            let i3 = buf.getNumber(NumberFormat.UInt16BE, 4) & 0xfff;
-            /**
-                        let l1 = buf.getNumber(NumberFormat.UInt16BE, 6) & 0xfff;
-                        let l2 = buf.getNumber(NumberFormat.UInt16BE, 8) & 0xfff;
-                        let l3 = buf.getNumber(NumberFormat.UInt16BE, 10) & 0xfff;
-                        let l4 = buf.getNumber(NumberFormat.UInt16BE, 12) & 0xfff;
-                        let l5 = buf.getNumber(NumberFormat.UInt16BE, 14) & 0xfff;
-             */
-            serial.writeLine(i1 + " " + i2 + " " + i3);
+            // let i1 = buf.getNumber(NumberFormat.UInt16BE, 0) & 0xfff;
+            // let i2 = buf.getNumber(NumberFormat.UInt16BE, 2) & 0xfff;
+            // let i3 = buf.getNumber(NumberFormat.UInt16BE, 4) & 0xfff;
+
+            let l1 = buf.getNumber(NumberFormat.UInt16BE, 0) & 0xfff;
+            let l2 = buf.getNumber(NumberFormat.UInt16BE, 2) & 0xfff;
+            let l3 = buf.getNumber(NumberFormat.UInt16BE, 4) & 0xfff;
+            let l4 = buf.getNumber(NumberFormat.UInt16BE, 6) & 0xfff;
+            let l5 = buf.getNumber(NumberFormat.UInt16BE, 8) & 0xfff;
+
+            serial.writeLine( l1 + " " + l2 + " " + l3 + " " + l4 + " " + l5);
 
             basic.pause(100);
         }
